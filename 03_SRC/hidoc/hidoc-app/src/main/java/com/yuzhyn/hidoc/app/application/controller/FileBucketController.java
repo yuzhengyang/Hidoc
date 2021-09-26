c20f610a9f9e27365d8f3f227746ab16729508b9
于正洋 <yuzhyn@163.com>
2021-09-26 18:13:02 +0800

    test

--- 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/controller/FileBucketController.java
+++ 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/controller/FileBucketController.java
@@ -1,15 +1,22 @@
 package com.yuzhyn.hidoc.app.application.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.yuzhyn.azylee.core.datas.collections.ListTool;
+import com.yuzhyn.azylee.core.datas.ids.UUIDTool;
 import com.yuzhyn.azylee.core.datas.regexs.RegexPattern;
+import com.yuzhyn.azylee.core.ios.dirs.DirTool;
 import com.yuzhyn.hidoc.app.aarg.R;
 import com.yuzhyn.hidoc.app.application.entity.file.FileBucket;
 import com.yuzhyn.hidoc.app.application.entity.file.FileCursor;
 import com.yuzhyn.hidoc.app.application.entity.file.FileCursorView;
+import com.yuzhyn.hidoc.app.application.entity.sys.SysAccessLog;
 import com.yuzhyn.hidoc.app.application.mapper.file.FileBucketMapper;
 import com.yuzhyn.hidoc.app.application.mapper.file.FileCursorMapper;
 import com.yuzhyn.hidoc.app.application.mapper.file.FileCursorViewMapper;
+import com.yuzhyn.hidoc.app.application.mapper.sys.SysAccessLogMapper;
 import com.yuzhyn.hidoc.app.common.model.ResponseData;
 import com.yuzhyn.hidoc.app.manager.CurrentUserManager;
 import lombok.extern.slf4j.Slf4j;
--- 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/controller/TestController.java
+++ 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/controller/TestController.java
@@ -1,25 +1,40 @@
-package com.yuzhyn.hidoc.app.unuse;
+package com.yuzhyn.hidoc.app.application.controller;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.yuzhyn.azylee.core.datas.collections.MapTool;
 import com.yuzhyn.azylee.core.datas.ids.UUIDTool;
+import com.yuzhyn.azylee.core.datas.regexs.RegexPattern;
 import com.yuzhyn.azylee.core.ios.dirs.DirTool;
 import com.yuzhyn.hidoc.app.aarg.R;
+import com.yuzhyn.hidoc.app.application.entity.file.FileBucket;
+import com.yuzhyn.hidoc.app.application.entity.file.FileCursor;
+import com.yuzhyn.hidoc.app.application.entity.file.FileCursorView;
 import com.yuzhyn.hidoc.app.application.entity.sys.SysAccessLog;
+import com.yuzhyn.hidoc.app.application.mapper.file.FileBucketMapper;
+import com.yuzhyn.hidoc.app.application.mapper.file.FileCursorMapper;
+import com.yuzhyn.hidoc.app.application.mapper.file.FileCursorViewMapper;
 import com.yuzhyn.hidoc.app.application.mapper.sys.SysAccessLogMapper;
 import com.yuzhyn.hidoc.app.common.model.ResponseData;
+import com.yuzhyn.hidoc.app.manager.CurrentUserManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class FileTestController {
+@Slf4j
+@RestController
+@RequestMapping("test")
+public class TestController {
 
 
     @Autowired
     SysAccessLogMapper sysAccessLogMapper;
-
 
     @GetMapping({"export"})
     public ResponseData export() {
