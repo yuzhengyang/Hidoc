e6674760bec30f557edad7e22272a4239eb64d6b
yuzhengyang <inc@live.cn>
2021-10-05 20:40:56 +0800

    数据收集接口及数据解析（PG库JSON类型处理）

--- 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/common/constant/UrlAccess.java
+++ 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/common/constant/UrlAccess.java
@@ -14,15 +14,25 @@ public class UrlAccess {
 
     public static List<String> anonymous() {
         List<String> urls = new ArrayList<>();
+        // 用户注册
         urls.add("/user/register");
+        // 用户登录
         urls.add("/user/login");
-//        urls.add("/user/logout");
+        // 用户登出
+        urls.add("/user/logout");
+        // 文集（公开文集列表）
         urls.add("/collected/preview");
-        urls.add("/collected/test");
-        urls.add("/collected/test2");
+        // 文集详情
         urls.add("/collected/get");
+        // 文档详情
         urls.add("/doc/get");
+        // 文件下载
         urls.add("/f/d");
+        // 数据收集器接入
+        urls.add("/datacoll/create");
+        // 其他测试入口
+//        urls.add("/collected/test");
+//        urls.add("/collected/test2");
         return urls;
     }
 }
