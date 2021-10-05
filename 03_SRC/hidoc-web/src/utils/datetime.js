037e8477bb5700a5224ee02c72328e8cb432a2d6
yuzhengyang <inc@live.cn>
2021-10-05 20:41:26 +0800

    运行及配置信息

--- 03_SRC/hidoc-web/src/utils/datetime.js
+++ 03_SRC/hidoc-web/src/utils/datetime.js
@@ -36,3 +36,17 @@ export function datetimeDesc(valueTime) {
         }
     }
 }
+export function datetimeFormat(valueTime, fmt) {
+    var o = {
+        'M+': valueTime.getMonth() + 1, //月份
+        'd+': valueTime.getDate(), //日
+        'H+': valueTime.getHours(), //小时
+        'm+': valueTime.getMinutes(), //分
+        's+': valueTime.getSeconds(), //秒
+        'q+': Math.floor((valueTime.getMonth() + 3) / 3), //季度
+        S: valueTime.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (valueTime.getFullYear() + '').substr(4 - RegExp.$1.length));
+    for (var k in o) if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+    return fmt;
+}
