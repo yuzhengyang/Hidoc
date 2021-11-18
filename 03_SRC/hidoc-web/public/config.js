198c7455c6ab201bfe3bee0c4aee0d983ed71557
yuzhengyang <inc@live.cn>
2021-11-18 22:21:12 +0800

    在家的配置

--- 03_SRC/hidoc-web/public/config.js
+++ 03_SRC/hidoc-web/public/config.js
@@ -1,9 +1,9 @@
-// var config = {
-//     author: 'yuzhengyang',
-//     environment: '家',
-//     baseServer: 'http://192.168.3.128:24001/',
-//     hdImageFlag: '#hd.image->'
-// };
+var config = {
+    author: 'yuzhengyang',
+    environment: '家',
+    baseServer: 'http://192.168.3.128:24001/',
+    hdImageFlag: '#hd.image->'
+};
 
 // var config = {
 //     author: 'yuzhengyang',
@@ -12,9 +12,9 @@
 //     hdImageFlag: '#hd.image->'
 // };
 
-var config = {
-    author: 'yuzhengyang',
-    environment: '公司私有',
-    baseServer: 'http://10.16.12.106:24001/',
-    hdImageFlag: '#hd.image->'
-};
+// var config = {
+//     author: 'yuzhengyang',
+//     environment: '公司私有',
+//     baseServer: 'http://10.16.12.106:24001/',
+//     hdImageFlag: '#hd.image->'
+// };
