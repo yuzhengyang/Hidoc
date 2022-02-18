2ade32199ecd000d05048d9303d54802238d41d4
于正洋 <yuzhyn@163.com>
2022-02-18 18:14:27 +0800

    访问日志增强

--- 03_SRC/hidoc/hidoc-app/README-DBUPDATE.md
+++ 03_SRC/hidoc/hidoc-app/README-DBUPDATE.md
@@ -37,3 +37,29 @@ ALTER TABLE public.doc_access_log
     OWNER to postgres;
 
 ```
+
+## 2022年2月15日
+```
+DROP TABLE `sys_access_log`;
+CREATE TABLE `sys_access_log` (
+  `id` varchar(20) NOT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `begin_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `uri` varchar(1024) DEFAULT NULL,
+  `method` varchar(20) DEFAULT NULL,
+  `elapsed_time` bigint DEFAULT NULL,
+  `user_id` varchar(20) DEFAULT NULL,
+  `step` varchar(200) DEFAULT NULL,
+  `begin_max_memory` bigint DEFAULT NULL,
+  `begin_total_memory` bigint DEFAULT NULL,
+  `begin_free_memory` bigint DEFAULT NULL,
+  `end_max_memory` bigint DEFAULT NULL,
+  `end_total_memory` bigint DEFAULT NULL,
+  `end_free_memory` bigint DEFAULT NULL,
+  `probably_use_memory` bigint DEFAULT NULL,
+  `thread_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `exception` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+```
--- 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/entity/sys/SysAccessLog.java
+++ 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/entity/sys/SysAccessLog.java
@@ -14,50 +14,20 @@ import java.time.LocalDateTime;
 public class SysAccessLog {
     private String id;
     private String ip;
-    private LocalDateTime createTime;
+    private LocalDateTime beginTime;
+    private LocalDateTime endTime;
     private String uri;
     private String method;
     private Long elapsedTime;
     private String userId;
-
-    @TableField(exist = false)
-    private String s1;
-    @TableField(exist = false)
-    private String s2;
-    @TableField(exist = false)
-    private String s3;
-    @TableField(exist = false)
-    private String s4;
-    @TableField(exist = false)
-    private String s5;
-    @TableField(exist = false)
-    private String s6;
-    @TableField(exist = false)
-    private String s7;
-    @TableField(exist = false)
-    private String s8;
-    @TableField(exist = false)
-    private String s9;
-    @TableField(exist = false)
-    private String s10;
-    @TableField(exist = false)
-    private String s11;
-    @TableField(exist = false)
-    private String s12;
-    @TableField(exist = false)
-    private String s13;
-    @TableField(exist = false)
-    private String s14;
-    @TableField(exist = false)
-    private String s15;
-    @TableField(exist = false)
-    private String s16;
-    @TableField(exist = false)
-    private String s17;
-    @TableField(exist = false)
-    private String s18;
-    @TableField(exist = false)
-    private String s19;
-    @TableField(exist = false)
-    private String s20;
+    private String step;
+    private String threadName;
+    private Long beginMaxMemory;
+    private Long beginTotalMemory;
+    private Long beginFreeMemory;
+    private Long endMaxMemory;
+    private Long endTotalMemory;
+    private Long endFreeMemory;
+    private Long probablyUseMemory;
+    private String exception;
 }
