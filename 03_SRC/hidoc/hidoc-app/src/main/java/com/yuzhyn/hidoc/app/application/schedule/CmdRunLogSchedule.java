ee961bf05b93ec43e45f907e3a671ea5952758da
yuzhengyang <inc@live.cn>
2022-09-14 18:28:29 +0800

    test schedule

---
+++ 03_SRC/hidoc/hidoc-app/src/main/java/com/yuzhyn/hidoc/app/application/schedule/CmdRunLogSchedule.java
@@ -0,0 +1,83 @@
+package com.yuzhyn.hidoc.app.application.schedule;
+
+import com.yuzhyn.azylee.core.datas.collections.MapTool;
+import com.yuzhyn.azylee.core.datas.datetimes.DateTimeFormat;
+import com.yuzhyn.azylee.core.datas.datetimes.DateTimeFormatPattern;
+import com.yuzhyn.azylee.core.datas.datetimes.LocalDateTimeTool;
+import com.yuzhyn.azylee.core.datas.exceptions.ExceptionTool;
+import com.yuzhyn.azylee.core.datas.strings.StringConst;
+import com.yuzhyn.azylee.core.ios.dirs.DirTool;
+import com.yuzhyn.azylee.core.ios.txts.PropertyTool;
+import com.yuzhyn.azylee.core.ios.txts.TxtTool;
+import com.yuzhyn.azylee.core.systems.bases.SystemStatusTool;
+import com.yuzhyn.azylee.core.systems.models.SystemStatusInfo;
+import com.yuzhyn.hidoc.app.aarg.R;
+import com.yuzhyn.hidoc.app.application.entity.doc.DocAccessLog;
+import com.yuzhyn.hidoc.app.application.entity.serverman.ServerManExeLog;
+import com.yuzhyn.hidoc.app.application.entity.sys.SysAccessLog;
+import com.yuzhyn.hidoc.app.application.entity.sys.SysStatusLog;
+import com.yuzhyn.hidoc.app.application.mapper.doc.DocAccessLogMapper;
+import com.yuzhyn.hidoc.app.application.mapper.serverman.ServerManExeLogMapper;
+import com.yuzhyn.hidoc.app.application.mapper.sys.SysAccessLogMapper;
+import com.yuzhyn.hidoc.app.application.mapper.sys.SysStatusLogMapper;
+import com.yuzhyn.hidoc.app.application.model.serverman.CmdRunLog;
+import com.yuzhyn.hidoc.app.utils.ssh.SshClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@EnableScheduling
+@EnableAsync
+public class CmdRunLogSchedule {
+
+    @Autowired
+    SysAccessLogMapper sysAccessLogMapper;
+
+    @Autowired
+    DocAccessLogMapper docAccessLogMapper;
+
+    @Autowired
+    SysStatusLogMapper sysStatusLogMapper;
+
+    @Autowired
+    ServerManExeLogMapper serverManExeLogMapper;
+
+
+//    @Async // 是否等待上一线程执行完毕再执行，使用是不等待，直接创建执行，会产生并行执行
+    @Scheduled(cron = "*/2 * * * * ?")
+    public void job1() {
+        Map<String, List<String>> groupMap = new HashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            CmdRunLog cmdRunLog = R.Queues.CmdRunLogQueue.poll();
+            if (cmdRunLog == null) break;
+
+            if (!groupMap.containsKey(cmdRunLog.getDialogId())) {
+                groupMap.put(cmdRunLog.getDialogId(), new ArrayList<>());
+            }
+
+            groupMap.get(cmdRunLog.getDialogId()).add(new String(cmdRunLog.getTextBytes()));
+        }
+
+        for (String dialogId : groupMap.keySet()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String text : groupMap.get(dialogId)) {
+                stringBuilder.append(text);
+                stringBuilder.append(StringConst.NEWLINE);
+            }
+            String file = DirTool.combine(R.Paths.ServerManSsh, dialogId + ".txt");
+            TxtTool.append(file, stringBuilder.toString());
+        }
+    }
+}
