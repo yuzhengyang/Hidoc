45b454ddc29ddbac2d1f83cc0292c1dac7dcedd5
yuzhengyang <inc@live.cn>
2022-11-09 22:26:11 +0800

    前端引用调整，使用新的vue-icon组件

--- 03_SRC/hidoc-web/src/main.js
+++ 03_SRC/hidoc-web/src/main.js
@@ -93,6 +93,10 @@ VMdEditor.use(createAlignPlugin());
 // 百度图表EChart
 import * as echarts from 'echarts';
 
+// 导入所有图标并进行全局注册
+// 如果您正在使用CDN引入，请删除下面一行
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
@@ -101,3 +105,6 @@ app.use(VMdEditor);
 app.use(VMdPreview);
 app.config.globalProperties.$echarts = echarts;
 app.mount('#app');
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
