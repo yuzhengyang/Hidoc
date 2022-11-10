789c50116c9ef73ed0830625bcc8e9063aa9b646
yuzhengyang <inc@live.cn>
2022-11-10 16:41:00 +0800

    增加模板类型，支撑模板功能

--- 02_DB/02_建表脚本/02_维护表_doc_collected.sql
+++ 02_DB/02_建表脚本/02_维护表_doc_collected.sql
@@ -11,11 +11,23 @@ CREATE TABLE IF NOT EXISTS public.doc_collected (
 	create_user_id varchar(20) NULL,
 	owner_user_id varchar(20) NULL,
 	create_time timestamp NULL,
-	is_open bool NULL,
-	is_delete bool NULL,
+	is_login_access bool DEFAULT false,
+	is_open bool DEFAULT false,
+	is_delete bool DEFAULT false,
+	is_templet bool DEFAULT false,
 	CONSTRAINT doc_collected_pkey PRIMARY KEY (id)
 );
 
 
-SELECT * FROM sysdb_add_column('doc_collected', 'is_login_access', 'bool NULL');
+SELECT * FROM sysdb_add_column('doc_collected', 'is_login_access', 'bool DEFAULT false');
 SELECT * FROM sysdb_add_column('doc_collected', 'password_access', 'varchar(255) NULL');
+SELECT * FROM sysdb_add_column('doc_collected', 'is_templet', 'bool DEFAULT false');
+
+
+UPDATE doc_collected SET is_open = false WHERE is_open IS NULL;
+UPDATE doc_collected SET is_delete = false WHERE is_delete IS NULL;
+UPDATE doc_collected SET is_templet = false WHERE is_templet IS NULL;
+UPDATE doc_collected SET is_login_access = false WHERE is_login_access IS NULL;
+
+
+
