3c72aaee0e2c08405ddeff524f9fe8f2ae3a436d
yuzhengyang <inc@live.cn>
2025-11-26 13:40:28 +0800

    临时测试Es工具类

--- 03_SRC/hidoc/hidoc-app/src/test/java/com/yuzhyn/hidoc/app/EsTest.java
+++ 03_SRC/hidoc/hidoc-app/src/test/java/com/yuzhyn/hidoc/app/EsTest.java
@@ -13,27 +13,31 @@ import java.util.Map;
 
 public class EsTest {
     public static void main(String[] args) throws IOException {
-        String index = "test.102";
-        EsTool esTool = new EsTool("https://www.yuzhyn.top:33001", "elastic", "x5676bca-aOEqBZz28K5", "hidoc.");
-        esTool.createClient();
+        try{
+            String index = "test.103";
+            EsTool esTool = new EsTool("https://192.168.14.184:9004", "elastic", "x5676bca-aOEqBZz28K5", "hidoc.");
+            esTool.createClient();
 
-        Map<String, Property> properties = new HashMap<>();
-        properties.put("location", Property.of(p -> p.geoPoint(GeoPointProperty.of(g -> g))));
-        esTool.createIndex(index, properties);
+            Map<String, Property> properties = new HashMap<>();
+            properties.put("location", Property.of(p -> p.geoPoint(GeoPointProperty.of(g -> g))));
+            esTool.createIndex(index, properties);
 
-        String id = String.valueOf(System.currentTimeMillis());
-        esTool.createDocument(index, id, Map.of(
-                "name", "test",
-                "location", "30.0,120.0",
-                "createTime", DateTimeFormat.toStr(LocalDateTime.now(), DateTimeFormatPattern.NORMAL_DATETIME_TIMEZONE)));
+            String id = String.valueOf(System.currentTimeMillis());
+            esTool.createDocument(index, id, Map.of(
+                    "name", "test",
+                    "location", "30.0,120.0",
+                    "createTime", DateTimeFormat.toStr(LocalDateTime.now(), DateTimeFormatPattern.NORMAL_DATETIME_TIMEZONE)));
 
-        esTool.createIndex(index, properties);
+            esTool.createIndex(index, properties);
 
-        id = String.valueOf(System.currentTimeMillis());
-        esTool.createDocument(index, id, Map.of(
-                "name", "test",
-                "location", "40.0,120.0",
-                "createTime", DateTimeFormat.toStr(LocalDateTime.now(), DateTimeFormatPattern.NORMAL_DATETIME_TIMEZONE)));
+            id = String.valueOf(System.currentTimeMillis());
+            esTool.createDocument(index, id, Map.of(
+                    "name", "test",
+                    "location", "40.0,120.0",
+                    "createTime", DateTimeFormat.toStr(LocalDateTime.now(), DateTimeFormatPattern.NORMAL_DATETIME_TIMEZONE)));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
